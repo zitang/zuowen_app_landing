30c8a8586187a0352768f0ccea3fa5811b2f568c
Zixiang Tang <safari.xjtu@gmail.com>
2014-10-28 23:30:41 +0800

    show links

--- js/landing.js
+++ js/landing.js
@@ -1,14 +1,16 @@
 $(function() {
     var channel = getParameterByName('source').toLowerCase();
 
-    if (channel != 'weixin' && channel != 'qzone' && channel != 'qq' && channel != 'magic') {
-        channel = 'magic';
+    if (list.indexOf(channel) === -1) {
+        channel = 'zuowen';
     }
 
     $('#downloadLink').attr('href', BASE_LINK.replace('{channel}', channel));
     $('#mDownloadLink').attr('href', BASE_LINK.replace('{channel}', 'm_' + channel));
     $('#codeImg').attr('src', CODE_IMG.replace('{channel}', channel));
 });
+
+var list = ['weixin', 'qzone', 'qq', 'magic', 'zuowen'];
 
 var BASE_LINK = 'http://atth.zuowen.com/app/zuowen_{channel}.apk';
 var CODE_IMG = 'images/qrcode_{channel}.png';
